ad18cd93569cbfe0793ea01dfa29768b084d8d1b
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-23 23:48:31 -0300

    Some CSS style in main menu

--- src/views/site/menu.php
+++ src/views/site/menu.php
@@ -1,4 +1,13 @@
-<menu>
+<style>
+    .main-menu>li {
+        display: inline;
+        padding-left: 3rem;
+        padding-right: 3rem;
+        background-color: lightgray;
+    }
+</style>
+
+<menu class="main-menu">
     <li>Início</li>
     <li>Contato</li>
     <li>Estudos</li>
