488d21dfb196b4f79336339e59c6f9d21c278da1
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-29 23:00:50 -0300

    Using routes to instance a controller

--- public/index.php
+++ public/index.php
@@ -2,6 +2,20 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// get configured routes to controllers
+$routes = require_once __DIR__ . '/../src/config/routes.php';
+
+// get route
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$controllerClass = $routes["$httpMethod|$pathInfo"];
+echo $controllerClass;
+$controller = new $controllerClass();
+
+var_dump($controller);
+exit;
+
 $pageTitle = "Site pessoal";
 require_once __DIR__ . '/../src/views/layout/header.php';
 ?>
