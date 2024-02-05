c2b7c2fdc48f5de1e2c6d67d33aa58e6d3ae48f5
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-04 21:25:37 -0300

    Method execute example in ErrorController

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,4 +4,8 @@ namespace FabioSchunig\Site\Controller;
 
 class ErrorController implements Controller
 {
+    public function execute(): void
+    {
+        echo "ErrorController execute";
+    }
 }
