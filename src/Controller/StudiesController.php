01592055e77e7866c8f1004708a8212fabc631cb
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-06 22:50:14 -0300

    StudiesController class

---
+++ src/Controller/StudiesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FabioSchunig\Site\Controller;
+
+class StudiesController implements Controller
+{
+    public function execute(): void
+    {
+        echo "StudiesController execute";
+    }
+}
