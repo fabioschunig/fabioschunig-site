3a2ac602dd1589d80ee454780effd58aadc07f1b
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-06 23:38:48 -0300

    Moving render to BaseController

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,6 +6,19 @@ abstract class BaseController implements Controller
 {
     public function execute(): void
     {
-        echo "BaseController execute";
+        $this->render();
+    }
+
+    protected function render(): void
+    {
+        $pageTitle = "Site pessoal";
+        require_once __DIR__ . '/../views/layout/header.php';
+
+        echo '<body>';
+        require_once __DIR__ . '/../views/site/menu.php';
+        require_once __DIR__ . '/../views/site/main.php';
+        echo '</body>';
+
+        require_once __DIR__ . '/..//views/layout/footer.php';
     }
 }
--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -6,19 +6,6 @@ class SiteController extends BaseController implements Controller
 {
     public function execute(): void
     {
-        $this->render();
-    }
-
-    private function render(): void
-    {
-        $pageTitle = "Site pessoal";
-        require_once __DIR__ . '/../views/layout/header.php';
-
-        echo '<body>';
-        require_once __DIR__ . '/../views/site/menu.php';
-        require_once __DIR__ . '/../views/site/main.php';
-        echo '</body>';
-
-        require_once __DIR__ . '/..//views/layout/footer.php';
+        parent::execute();
     }
 }
