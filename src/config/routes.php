4fc44affe79694ad8da32af5ed545d72c1f58b89
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-06 22:48:40 -0300

    Route to ContactController

--- src/config/routes.php
+++ src/config/routes.php
@@ -3,4 +3,5 @@
 return [
     'GET|/' => \FabioSchunig\Site\Controller\SiteController::class,
     'GET|/about' => \FabioSchunig\Site\Controller\AboutController::class,
+    'GET|/contact' => \FabioSchunig\Site\Controller\ContactController::class,
 ];
