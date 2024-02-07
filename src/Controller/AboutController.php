448791e1d2cf402905b68178bc97cce6f36cd5ff
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-06 22:45:04 -0300

    AboutController class

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FabioSchunig\Site\Controller;
+
+class AboutController implements Controller
+{
+    public function execute(): void
+    {
+        echo "AboutController execute";
+    }
+}
