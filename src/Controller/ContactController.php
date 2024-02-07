015cad49e782116c0de0b6f1b181a2a5e109fef4
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-06 22:48:03 -0300

    ContactController class

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FabioSchunig\Site\Controller;
+
+class ContactController implements Controller
+{
+    public function execute(): void
+    {
+        echo "ContactController execute";
+    }
+}
