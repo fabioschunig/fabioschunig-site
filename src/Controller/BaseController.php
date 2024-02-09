7889b1fda55a06ccb7bebaf06a2c568dcbc0cf44
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-08 21:41:55 -0300

    Method to build HTML and render a view file

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,21 +6,21 @@ abstract class BaseController implements Controller
 {
     private string $viewPath = __DIR__ . '/../views/';
 
-    public function execute(): void
-    {
-        $this->render();
-    }
-
-    protected function render(): void
+    private function buildHtml(string $view): void
     {
         $pageTitle = "Site pessoal";
-        require_once $this->viewPath . 'layout/header.php';
+        require_once $this->viewPath . "layout/header.php";
 
         echo '<body>';
-        require_once $this->viewPath . 'site/menu.php';
-        require_once $this->viewPath . 'site/main.php';
+        require_once $this->viewPath . "site/menu.php";
+        require_once $this->viewPath . "site/$view.php";
         echo '</body>';
 
-        require_once $this->viewPath . 'layout/footer.php';
+        require_once $this->viewPath . "layout/footer.php";
+    }
+
+    protected function render(string $view): void
+    {
+        $this->buildHtml($view);
     }
 }
