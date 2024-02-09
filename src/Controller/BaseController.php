c35a5fa4a441c9c6a7e9133256a22beecc959505
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-08 21:13:24 -0300

    Constant to Views directory

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,6 +4,8 @@ namespace FabioSchunig\Site\Controller;
 
 abstract class BaseController implements Controller
 {
+    private string $viewPath = __DIR__ . '/../views/';
+
     public function execute(): void
     {
         $this->render();
@@ -12,13 +14,13 @@ abstract class BaseController implements Controller
     protected function render(): void
     {
         $pageTitle = "Site pessoal";
-        require_once __DIR__ . '/../views/layout/header.php';
+        require_once $this->viewPath . 'layout/header.php';
 
         echo '<body>';
-        require_once __DIR__ . '/../views/site/menu.php';
-        require_once __DIR__ . '/../views/site/main.php';
+        require_once $this->viewPath . 'site/menu.php';
+        require_once $this->viewPath . 'site/main.php';
         echo '</body>';
 
-        require_once __DIR__ . '/..//views/layout/footer.php';
+        require_once $this->viewPath . 'layout/footer.php';
     }
 }
