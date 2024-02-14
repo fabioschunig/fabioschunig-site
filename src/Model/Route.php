a82061686cbd0bc1e78e9816badfdbb249d71052
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-13 23:22:44 -0300

    Method getPathInfo to handle different Apache versions

--- src/Model/Route.php
+++ src/Model/Route.php
@@ -16,7 +16,7 @@ class Route
     public function getRouteControllerClass(): string
     {
         // mount route
-        $pathInfo = $this->server['PATH_INFO'] ?? '/';
+        $pathInfo = $this->getPathInfo();
         $httpMethod = $this->server['REQUEST_METHOD'];
 
         // checks if route exists
@@ -28,5 +28,24 @@ class Route
         }
 
         return $controllerClass;
+    }
+
+    private function getPathInfo(): string
+    {
+        if (isset($this->server['PATH_INFO'])) {
+            return $this->server['PATH_INFO'];
+        }
+
+        if (isset($this->server['REQUEST_URI'])) {
+            $requestUri = $this->server['REQUEST_URI'];
+            $baseUrl = $requestUri;
+            if (str_contains($requestUri, '?')) {
+                $baseUrl = explode('?', $requestUri)[0];
+            }
+            $prefix = $this->server['CONTEXT_PREFIX'] ?? '';
+            return str_replace($prefix, '', $baseUrl);
+        }
+
+        return '/';
     }
 }
