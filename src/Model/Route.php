22535a6c6bcc7247eea5c17174de9977b668a03f
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-13 22:18:21 -0300

    Route class constructor with dependencies params

--- src/Model/Route.php
+++ src/Model/Route.php
@@ -4,4 +4,12 @@ namespace FabioSchunig\Site\Model;
 
 class Route
 {
+    private array $server;
+    private array $routes;
+
+    public function __construct(array $server, array $routes)
+    {
+        $this->server = $server;
+        $this->routes = $routes;
+    }
 }
