ac271d8307058086d4842df96d5e5298eae16b12
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-17 22:14:35 -0300

    meta author

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="description" content="Site pessoal Fabio Schunig">
     <meta name="keywords" content="software, developer">
+    <meta name="author" content="Fabio Schunig">
 
     <link rel="stylesheet" href="css/main.css">
 </head>
