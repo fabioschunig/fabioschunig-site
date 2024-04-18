b35ceeb27ee3246220f0599de4755e053b5195c9
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-17 22:13:54 -0300

    meta keywords

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -10,6 +10,7 @@
 
     <meta charset="UTF-8">
     <meta name="description" content="Site pessoal Fabio Schunig">
+    <meta name="keywords" content="software, developer">
 
     <link rel="stylesheet" href="css/main.css">
 </head>
