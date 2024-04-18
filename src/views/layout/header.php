fcb03f6495997dab794069e56caab14ccb0daf37
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-17 22:16:42 -0300

    meta viewport

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -12,6 +12,7 @@
     <meta name="description" content="Site pessoal Fabio Schunig">
     <meta name="keywords" content="software, developer">
     <meta name="author" content="Fabio Schunig">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/main.css">
 </head>
