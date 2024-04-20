0a658f581e330bec57ccee34049617387b52a4be
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-19 21:46:48 -0300

    Adicionando JS do Bootstrap 5.3

--- src/views/layout/main.php
+++ src/views/layout/main.php
@@ -9,6 +9,9 @@ require_once 'head.php';
     <?php require_once $this->viewPath . "site/menu.php"; ?>
 
     <?= $content ?>
+
+    <!-- Bootstrap 5.3 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 <?php
