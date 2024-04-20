6d4977e7ff18ae960b35749d1df1e8fce7d7f656
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-19 21:56:18 -0300

    Footer com estilos Bootstrap

--- src/views/layout/footer.php
+++ src/views/layout/footer.php
@@ -1,4 +1,3 @@
-<footer>
-    <hr>
-    <p>2014-2024 - Fabio Schunig</p>
+<footer id="footer" class="mt-1 bg-light border-top">
+    <p class="m-1 text-center">2014-2024 - Fabio Schunig</p>
 </footer>
