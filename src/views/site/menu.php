17b7148d12144f4f46dc0282843dc3746ffc7768
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-24 23:16:52 -0300

    Painel no topo da página

--- src/views/site/menu.php
+++ src/views/site/menu.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg container bg-secondary bg-gradient rounded-1">
+<nav class="navbar navbar-expand-lg container fixed-top bg-secondary bg-gradient rounded-1">
     <div class="container-fluid">
         <a class="navbar-brand px-3" href=".">Início</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,3 +19,7 @@
         </div>
     </div>
 </nav>
+
+<div class="container painel">
+    Painel
+</div>
