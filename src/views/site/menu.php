6b3436981118d1a3b1e885f760a629c3c732e605
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-24 21:42:56 -0300

    Barra de navegação responsiva

--- src/views/site/menu.php
+++ src/views/site/menu.php
@@ -1,6 +1,21 @@
-<menu class="main-menu">
-    <li><a href=".">Início</a></li>
-    <li><a href="contact">Contato</a></li>
-    <li><a href="studies">Estudos</a></li>
-    <li><a href="about">Sobre</a></li>
-</menu>
+<nav class="navbar navbar-expand-lg container bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href=".">Início</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="contact">Contato</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="studies">Estudos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">Sobre</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
