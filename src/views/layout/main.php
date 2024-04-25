3a4ce5e7ff761a35e9154c8fd9a2ba704d6354f0
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-24 21:27:32 -0300

    Ajustes no layout principal para ocupar toda tela

--- src/views/layout/main.php
+++ src/views/layout/main.php
@@ -1,21 +1,25 @@
 <!DOCTYPE HTML>
-<html>
+<html class="h-100">
 
 <?php
 require_once 'head.php';
 ?>
 
-<body>
+<body class="d-flex flex-column h-100">
     <?php require_once $this->viewPath . "site/menu.php"; ?>
 
-    <?= $content ?>
+    <main id="main" class="flex-shrink-0" role="main">
+        <div class="container-fluid">
+            <?= $content ?>
+        </div>
+    </main>
+
+    <?php
+    require_once 'footer.php';
+    ?>
 
     <!-- Bootstrap 5.3 -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
-<?php
-require_once 'footer.php';
-?>
 
 </html>
