00e2fe35f5cfae4f66f048605fb011e07e4d4302
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-24 22:37:01 -0300

    Footer class container

--- src/views/layout/footer.php
+++ src/views/layout/footer.php
@@ -1,3 +1,3 @@
-<footer id="footer" class="mt-auto bg-light border-top">
+<footer id="footer" class="container mt-auto bg-secondary bg-gradient rounded-1 border-top">
     <p class="m-1 text-center">2014-2024 - Fabio Schunig</p>
 </footer>
