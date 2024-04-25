5f22981f03ed7f9eb32f5b8c60685cc7f19f036c
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-24 21:24:32 -0300

    Footer no final da página

--- src/views/layout/footer.php
+++ src/views/layout/footer.php
@@ -1,3 +1,3 @@
-<footer id="footer" class="mt-1 bg-light border-top">
+<footer id="footer" class="mt-auto bg-light border-top">
     <p class="m-1 text-center">2014-2024 - Fabio Schunig</p>
 </footer>
