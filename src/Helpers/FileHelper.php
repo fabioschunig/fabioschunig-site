80a89c2933a63d4dd77008b520ff324a7e9dd8d7
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-26 21:38:27 -0300

    Função para buscar arquivos de imagens

--- src/Helpers/FileHelper.php
+++ src/Helpers/FileHelper.php
@@ -4,4 +4,8 @@ namespace FabioSchunig\Site\Helpers;
 
 class FileHelper
 {
+    public static function imageFiles(string $path): array
+    {
+        return preg_grep('~\.(jpeg|jpg|png)$~i', scandir($path));
+    }
 }
